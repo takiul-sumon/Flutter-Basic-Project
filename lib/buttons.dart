f07c907e4f11d87c246de6fa1e4f8373ab50134d
Md Takiul Islam Sumon <Takiulislam20@gmail.com>
2025-01-17 19:32:23 +0600

    Various button

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -16,17 +16,53 @@ class _ButtonWidgetState extends State<ButtomWidget> {
           children: [
             SizedBox(
               child: ElevatedButton(
-                  onPressed: () {
-                    print('object');
-                  },
-                  child: Card(
-                    child: Text('data'),
-                  )),
+                onPressed: () {
+                  print('object');
+                },
+                child: Card(
+                  child: Text('data'),
+                ),
+                style: ButtonStyle(),
+              ),
             ),
             SizedBox(
               height: 10,
             ),
-            Container(child: Card(child: Icon(Icons.heat_pump_sharp),))
+            Container(
+                child: Card(
+              child: Icon(Icons.heat_pump_sharp),
+            )),
+            IconButton(onPressed: () {}, icon: Text('data')),
+            OutlinedButton(onPressed: () {}, child: Text('data')),
+            TextButton(
+              onPressed: () {},
+              child: Text('data'),
+              style: ButtonStyle(),
+            ),
+            SizedBox(
+              height: 50,
+              width: 50,
+              child: Text('data'),
+            ),
+             ElevatedButton(
+                onPressed: () {
+                  print('object');
+                },
+                child: Text('data',style: TextStyle(color: Colors.black),),
+                style:ElevatedButton.styleFrom(
+backgroundColor: Colors.white,
+                  //
+foregroundColor: Colors.white,                 
+                  // onPrimary: Colors.white,
+                  shadowColor: Colors.white,
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: EdgeInsets.all(20),
+                  minimumSize: Size(100, 50),
+                ),
+              )
           ],
         ),
       ),
