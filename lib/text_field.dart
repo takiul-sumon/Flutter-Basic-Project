e5a4c612728638ae2f53c26af5369aaa9e956150
Md Takiul Islam Sumon <Takiulislam20@gmail.com>
2025-01-18 00:20:08 +0600

    text

--- lib/text_field.dart
+++ lib/text_field.dart
@@ -7,16 +7,32 @@ class TextFields extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: 'Enter your name',
-          labelText: 'Name',
-          labelStyle: TextStyle(color: Colors.black),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            gapPadding: 10
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Enter your name',
+              labelText: 'Name',
+              labelStyle: TextStyle(color: Colors.black),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+            ),
           ),
-        ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+            child: TextField(
+              
+              decoration: InputDecoration(
+                fillColor: Colors.amber,
+                  hintText: 'Enter Your Name',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.zero, gapPadding: 10)),
+            ),
+          )
+        ],
       ),
     ));
   }
