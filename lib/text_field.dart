65716d3734f3578082cf5cb89e25ed4c8d06711c
Md Takiul Islam Sumon <Takiulislam20@gmail.com>
2025-01-18 12:14:36 +0600

    text

--- lib/text_field.dart
+++ lib/text_field.dart
@@ -1,37 +1,76 @@
 import 'package:flutter/material.dart';
 
-class TextFields extends StatelessWidget {
+class TextFields extends StatefulWidget {
   const TextFields({super.key});
 
+  @override
+  State<TextFields> createState() => _TextFieldsState();
+}
+
+class _TextFieldsState extends State<TextFields> {
+  bool onpressure = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
       child: Column(
         children: [
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'Enter your name',
-              labelText: 'Name',
-              labelStyle: TextStyle(color: Colors.black),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+            child: TextFormField(
+              maxLength: 10,
+              decoration: InputDecoration(
+                hintText: 'Enter Your Name',
+                fillColor: Colors.amber,
+                
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.zero,
+                ),
+              ),
             ),
           ),
           SizedBox(
             height: 10,
           ),
           Padding(
-            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+            padding: const EdgeInsets.all(10.0),
             child: TextField(
-              
               decoration: InputDecoration(
-                fillColor: Colors.amber,
-                  hintText: 'Enter Your Name',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.zero, gapPadding: 10)),
+                hintText: 'Enter your name',
+                prefixIcon: Icon(Icons.mail),
+                labelText: 'Name',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.clear),
+                  onPressed: () {},
+                ),
+                // label: Text('Name'),
+                labelStyle: TextStyle(color: Colors.black),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(3)),
+              ),
             ),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              obscureText: onpressure,
+              decoration: InputDecoration(
+                hintText: 'Enter your Password',
+                prefixIcon: Icon(Icons.lock),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.hide_source),
+                  onPressed: () {
+                    setState(() {
+                      onpressure = !onpressure;
+                    });
+                  },
+                ),
+                labelStyle: TextStyle(color: Colors.black),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+              ),
+            ),
+          ),
         ],
       ),
     ));
